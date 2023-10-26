6441c12cb5f1dec923a3f44abebd7d9cd38b45d9
TreSet <hanneshekl@gmai.com>
2023-10-26 19:06:38 +0200

    Fixed a crash when starting in a server environment.

--- src/main/java/net/treset/vanillaconfig/VanillaConfigMod.java
+++ src/main/java/net/treset/vanillaconfig/VanillaConfigMod.java
@@ -1,6 +1,8 @@
 package net.treset.vanillaconfig;
 
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 import net.treset.vanillaconfig.config.managers.SaveLoadManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +12,10 @@ public class VanillaConfigMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		if(FabricLoader.getInstance().getEnvironmentType() == EnvType.SERVER) {
+			LOGGER.info("Environment is server, disabling VanillaConfig.");
+			return;
+		}
 		SaveLoadManager.init();
 	}
 }
