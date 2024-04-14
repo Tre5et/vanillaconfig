838383afa368a1496c783cd87a2c51639108290e
TreSet <hanneshekl@gmai.com>
2024-04-14 13:07:21 +0200

    Fixed an issue with keybinds

--- src/main/java/net/treset/vanillaconfig/mixin/KeyboardMixin.java
+++ src/main/java/net/treset/vanillaconfig/mixin/KeyboardMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Keyboard.class)
 public class KeyboardMixin {
 
-    @Inject(method = "onKey(JIIII)V", at = @At("TAIL"), cancellable = true)
+    @Inject(method = "onKey(JIIII)V", at = @At("HEAD"), cancellable = true)
     public void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo c) {
         KeybindTools.onKeyEvent(window, key, scancode, action, modifiers);
     }
